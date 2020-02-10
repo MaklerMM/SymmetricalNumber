aa558ed5b4f2c444f550cfee6c72b1b19a7b774e
Maciej Muchowicz <maciej.muchowicz@gmail.com>
2020-02-10 09:13:13 +0100

    ukonczony program badania symetrycznosci max 4 cyfrowej liczby

--- src/pl/info/makler/Main.java
+++ src/pl/info/makler/Main.java
@@ -6,37 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner myObj = new Scanner(System.in);
+	System.out.println("podaj liczbe maksymalnie czterocyfrowa: ");
 	String number = myObj.next();
 
-	switch (number.length()){
-        case 1:{
-            if (number == "0") {
-                System.out.println("1");
-                break;
-            }
-        }
-        case 2:{
-            if (number.charAt(0)=='0' && number.charAt(1)=='0'){
-                System.out.println("1");
-                break;
-            }
-        }
-        case 3:{
-            if (number.charAt(2)=='0' && number.charAt(0)==number.charAt(1)){
-                System.out.println("1");
-                break;
-            }
-        }
-        case 4:{
-            if (number.charAt(0)==number.charAt(3) && number.charAt(1)==number.charAt(2)){
-                System.out.println("1");
-                break;
-            }
+	if (number.length() < 5) {
 
+        switch (number.length()) {
+            case 1: {
+                if (number == "0") {
+                    System.out.println("1");
+                    break;
+                }
+            }
+            case 2: {
+                if (number.charAt(0) == '0' && number.charAt(1) == '0') {
+                    System.out.println("1");
+                    break;
+                }
+            }
+            case 3: {
+                if (number.charAt(2) == '0' && number.charAt(0) == number.charAt(1)) {
+                    System.out.println("1");
+                    break;
+                }
+            }
+            case 4: {
+                if (number.charAt(0) == number.charAt(3) && number.charAt(1) == number.charAt(2)) {
+                    System.out.println("1");
+                    break;
+                }
+
+            }
+            default: {
+                System.out.println("0");
+            }
         }
-        default: {
-            System.out.println("0");
         }
-    }
+    else System.out.println("podaj liczbe maksymalnie czterocyfrowa");
     }
 }
